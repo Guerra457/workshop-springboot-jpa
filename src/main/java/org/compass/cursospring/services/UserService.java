2e6488e29ce33d637f580f3306909595f273730e
Matheus Guerra <matheus4xp@gmail.com>
2025-01-21 15:16:05 -0300

    Service layer, component registration

---
+++ src/main/java/org/compass/cursospring/services/UserService.java
@@ -0,0 +1,26 @@
+package org.compass.cursospring.services;
+
+import org.compass.cursospring.entities.User;
+import org.compass.cursospring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
