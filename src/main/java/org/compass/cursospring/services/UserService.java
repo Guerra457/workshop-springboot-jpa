4ad32fd1f18bd6d44a0b319732cb2a35af6d5143
Matheus Guerra <matheus4xp@gmail.com>
2025-01-23 15:15:06 -0300

    Exception handling - delete

--- src/main/java/org/compass/cursospring/services/UserService.java
+++ src/main/java/org/compass/cursospring/services/UserService.java
@@ -2,9 +2,12 @@ package org.compass.cursospring.services;
 
 import org.compass.cursospring.entities.User;
 import org.compass.cursospring.repositories.UserRepository;
+import org.compass.cursospring.services.exceptions.DatabaseException;
 import org.compass.cursospring.services.exceptions.ResourceNotFoundException;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,7 +33,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
