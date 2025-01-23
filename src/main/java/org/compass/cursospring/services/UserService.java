4e192373b2a1fd5839e56c017073625c50db4e73
Matheus Guerra <matheus4xp@gmail.com>
2025-01-23 15:19:10 -0300

    Exception handling - update

--- src/main/java/org/compass/cursospring/services/UserService.java
+++ src/main/java/org/compass/cursospring/services/UserService.java
@@ -1,5 +1,6 @@
 package org.compass.cursospring.services;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.compass.cursospring.entities.User;
 import org.compass.cursospring.repositories.UserRepository;
 import org.compass.cursospring.services.exceptions.DatabaseException;
@@ -43,9 +44,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
