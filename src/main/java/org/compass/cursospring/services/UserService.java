e146f2cc399bd08145f1581ab2fb042974f2f92f
Matheus Guerra <matheus4xp@gmail.com>
2025-01-23 15:00:52 -0300

    Exception handling - findById

--- src/main/java/org/compass/cursospring/services/UserService.java
+++ src/main/java/org/compass/cursospring/services/UserService.java
@@ -2,6 +2,8 @@ package org.compass.cursospring.services;
 
 import org.compass.cursospring.entities.User;
 import org.compass.cursospring.repositories.UserRepository;
+import org.compass.cursospring.services.exceptions.ResourceNotFoundException;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
